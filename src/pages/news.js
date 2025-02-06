a2e1b933a3df5e62d8ef8699572e06a954c1db3d
Vansh Patel <patel.vansh1607@gmail.com>
2025-02-06 17:34:06 +0300

    lined css to js , changed h1, added functions NewsPage

---
+++ src/pages/news.js
@@ -0,0 +1,14 @@
+import HeaderComponent from "../components/headerComponent"
+import "./news.css"
+
+function NewsPage() {
+    return (
+        <div>
+            <HeaderComponent/>
+            <h1>News Page</h1>
+            {/* Footer Component */}
+        </div>
+    )
+}
+
+export default NewsPage
