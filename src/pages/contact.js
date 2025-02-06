f1aa91806392675eab3a6f96a3dcd7de83d6f798
Vansh Patel <patel.vansh1607@gmail.com>
2025-02-06 17:30:16 +0300

    added import to contact page from header

---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import HeaderComponent from "../components/headerComponent"
+import "./about.css"
+
+function ContactPage() {
+    return (
+        <div>
+            <HeaderComponent/>
+            <h1>About Us</h1>
+            {/* Footer Component */}
+        </div>
+    )
+}
+
+export default ContactPage
