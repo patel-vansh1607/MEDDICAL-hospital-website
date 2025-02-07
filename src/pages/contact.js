166a37ae8b5b56434abb6156d2db472051a68ba9
Vansh Patel <patel.vansh1607@gmail.com>
2025-02-07 10:01:54 +0300

    added h5 and h1

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,15 @@
 import HeaderComponent from "../components/headerComponent"
 import "./contact.css"
-import Icon from "../Images/contact.png"
+import HeaderImage from "../Images/contact.png"
 
 function ContactPage() {
     return (
         <div>
             <HeaderComponent/>
             <header>
-                <img src={Icon}></img>
+                <img src={HeaderImage}></img>
+                <h5>Home / Contact</h5>
+                <h1>Our Contacts</h1>
             </header>
             {/* Footer Component */}
         </div>
